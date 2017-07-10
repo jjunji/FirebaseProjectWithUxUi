d2b6a1531b847fa0e67184c8c3174f1cc57338c0
jjunji <jh.jun92@gmail.com>
2017-07-11 02:15:23 +0900

    OTL

---
+++ CLOOK/CLOOK/app/src/main/java/com/example/jhjun/clook/IntroActivity.java
@@ -0,0 +1,63 @@
+package com.example.jhjun.clook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class IntroActivity extends AppCompatActivity {
+
+    ImageView btnStart;
+    Spinner spinnerSex;
+    Spinner spinnerAge;
+
+    String position = ""; // 성별 스피너 눌린 위치 값
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        btnStart = (ImageView) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+                intent.putExtra("spinerPosition",position);
+                startActivity(intent);
+            }
+        });
+
+        spinnerSex = (Spinner) findViewById(R.id.spinnerSex);
+        spinnerAge = (Spinner) findViewById(R.id.spinnerAge);
+
+        String[] items = {"남자/여자", "남자", "여자"};
+        String[] ageitmes = {"10/20/30", "10", "20", "30"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        ArrayAdapter<String> age_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ageitmes);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        age_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerSex.setAdapter(adapter);
+        spinnerAge.setAdapter(age_adapter);
+
+        spinnerSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int p, long id) {
+                position = p+"";  // 스피너 클릭시 해당 포지션 값 기억.(bundle에 담아서 MainActivity.class로 보낼 것임.)
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+}
